a26b7f973a4e7252ce7b60390c93d55b772a7510
Git-leng <leng_bw@163.com>
2018-01-21 14:56:20 +0800

    公用模板

---
+++ template/init/instance.js
@@ -0,0 +1,9 @@
+import { createAPI } from '<%- $$.relative("util") %>'
+
+const baseUrl = {
+	development: '',
+	prepub: '',
+	production: ''
+}[process.env.NODE_ENV]
+
+export default createAPI(baseUrl)
